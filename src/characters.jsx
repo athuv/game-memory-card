8507cc3550d92e0ae67574ed77b23d027fb3a6ad
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-10-29 17:53:33 +0530

    feat: add level easy gameplay UI and click logics

---
+++ src/characters.jsx
@@ -0,0 +1,66 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const characters = [
+  {
+    id: uuidv4(),
+    name: 'Po',
+    src: '../public/images/po.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Tigress',
+    src: '../public/images/tigress.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Crane',
+    src: '../public/images/crane.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Kai',
+    src: '../public/images/kai.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Mantis',
+    src: '../public/images/mantis.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Mono',
+    src: '../public/images/mono.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Mr Ping',
+    src: '../public/images/mr_ping.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Oogway',
+    src: '../public/images/oogway.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Shifu',
+    src: '../public/images/shifu.jpg',
+    clicked: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Viper',
+    src: '../public/images/viper.jpg',
+    clicked: false,
+  },
+];
+
+export default characters;
--- src/components/easy/EasyCard.jsx
+++ src/components/easy/EasyCard.jsx
@@ -1,9 +1,126 @@
-export default function EasyCard() {
+import { useState, useEffect, useRef } from 'react';
+import characters from '../../characters';
+import Tilt from 'react-parallax-tilt';
+import { mdiCreditCardRefresh } from '@mdi/js';
+
+function Card({ character, onClick, isImageVisible, cardRefs }) {
   return (
-    <section>
-      <div>A</div>
-      <div>B</div>
-      <div>C</div>
+    <>
+      {character.map((item, index) => (
+        <div
+          ref={(element) => (cardRefs.current[index] = element)}
+          className={`card`}
+          key={item.id}
+          onClick={() => {
+            onClick(item.id);
+          }}
+        >
+          <Tilt className="card-container__card-wrapper">
+            <div className={`card__overlay `}>
+              {isImageVisible && (
+                <picture>
+                  <source srcSet={item.src} media="(min-width: 769px)" />
+
+                  <img
+                    className={`card__overlay--image`}
+                    srcSet={character.src}
+                    alt="overlay-image"
+                  />
+                </picture>
+              )}
+            </div>
+            <picture>
+              <source
+                srcSet="../../public/images/card.jpg"
+                media="(min-width: 769px)"
+              />
+
+              <img
+                className={`card-section__card-container__card`}
+                srcSet="../../public/images/card.jpg"
+                alt="logo"
+              />
+            </picture>
+          </Tilt>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default function EasyCard() {
+  const [allCharacters, setAllCharacters] = useState(characters);
+  const [randomCharacters, setRandomCharacters] = useState(getCharacters(3));
+  const [isFlipped, setIsFlipped] = useState(0);
+  const [isImageVisible, setIsImageVisible] = useState(true);
+  const [score, setScore] = useState(0);
+  const [isLostGame, setIslostGame] = useState(false);
+  const cardRefs = useRef([]);
+  const [initialized, setInitialized] = useState(false);
+
+  function getCharacters(count) {
+    const shuffledCharacters = allCharacters.sort(() => 0.5 - Math.random());
+    const randomCharacters = shuffledCharacters.slice(0, count);
+    return randomCharacters;
+  }
+
+  useEffect(() => {
+    if (initialized) {
+      isLostGame === false
+        ? cardRefs.current.forEach((card) => {
+            card.classList.contains('card__flipped')
+              ? card.classList.remove('card__flipped')
+              : card.classList.add('card__flipped');
+          })
+        : null;
+
+      isLostGame === false
+        ? setTimeout(() => {
+            setIsImageVisible(true);
+            setRandomCharacters(getCharacters(3));
+          }, 600)
+        : null;
+    } else {
+      setInitialized(true);
+    }
+  }, [isFlipped]);
+
+  function isLost(id) {
+    const clickedItem = allCharacters.find((character) => character.id === id);
+    clickedItem.clicked ? setIslostGame(true) : '';
+  }
+
+  function changeClickedStatus(id) {
+    isLostGame === false
+      ? setAllCharacters((prevData) =>
+          prevData.map((item) =>
+            item.id === id ? { ...item, clicked: true } : item,
+          ),
+        )
+      : null;
+  }
+
+  const handleTiltClick = (id) => {
+    setIsImageVisible(false);
+    isLost(id);
+    changeClickedStatus(id);
+    setIsFlipped(isFlipped === 0 ? 1 : 0);
+  };
+
+  return (
+    <section className="card-section">
+      <div className="card-section__card-container">
+        {isLostGame ? (
+          'lost'
+        ) : (
+          <Card
+            cardRefs={cardRefs}
+            onClick={handleTiltClick}
+            character={randomCharacters}
+            isImageVisible={isImageVisible}
+          />
+        )}
+      </div>
     </section>
   );
 }
